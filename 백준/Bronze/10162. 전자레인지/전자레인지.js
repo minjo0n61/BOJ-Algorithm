c98fbfba83d67458e57052c3b8043e757ecd6970
MinJoon_Choi <minjoon1998@gmail.com>
2000-10-17 10:41:00 +0000

    [Bronze III] Title: 전자레인지, Time: 168 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync("/dev/stdin").toString().trim();
+
+const A = 300;
+const B = 60;
+const C = 10;
+
+let answer = '';
+let temp = input;
+
+answer += `${parseInt(temp / A)} `;
+temp = temp % A;
+
+answer += `${parseInt(temp / B)} `;
+temp = temp % B;
+
+answer += `${parseInt(temp / C)}`;
+temp = temp % C;
+
+answer = temp !== 0 ? -1 : answer
+
+console.log(answer);
