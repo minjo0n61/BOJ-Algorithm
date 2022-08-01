7c6b23adb62a94d07575e1c2dc88725be4f1d431
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-01 22:21:13 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,8 +1,4 @@
-//배열안에 각각 대입해봐서 결과 출력
-//서로 다른 값 몇개인지=>나머지 다른 값이면 변수에 +1
-
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
-
 let arr = [];
 
 input.forEach(element => {
@@ -10,6 +6,5 @@ input.forEach(element => {
     if (arr.indexOf(num) === -1) {
         arr.push(num);
     }
-
 });
 console.log(arr.length);
