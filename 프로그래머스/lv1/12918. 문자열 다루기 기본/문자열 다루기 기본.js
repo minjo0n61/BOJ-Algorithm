4912f305199ed316757051675393f1ec92c19e21
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-02 09:53:00 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,8 +1,9 @@
 function solution(s) {
     let answer = true;
-    if(s.length !==4 && s.length !==6){return false;}
-    for (let i=0;i<s.length;i++){
-        if(isNaN(Number(s[i]))){return false;}
+    if(s.length !== 4 && s.length !==6){return false;}
+    
+    for (let i=0; i<s.length; i++){
+        if(isNaN(Number(s[i]))===true){return false}
     }
     return answer;
 }
