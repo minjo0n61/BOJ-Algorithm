85b3356c0c7a33a80911bb2fa66900622f7d66a9
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-02 12:05:57 +0900

    [Bronze III] Title: 오븐 시계, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -1,12 +1,10 @@
-const fs=require('fs');
-const input=fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const current = input[0].split(" ");
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const time = input[0].split(" ");
+let origin_h = Number(time[0]);
+let origin_m = Number(time[1]);
+let cook_time = Number(input[1]);
 
-const ct=parseInt(current[0]);
-const cm=parseInt(current[1]);
-const cookTime=parseInt(input[1]);
+let end_h = Math.trunc((origin_h * 60 + origin_m + cook_time) / 60);
+let end_m = (origin_h * 60 + origin_m + cook_time) % 60;
 
-const et=Math.floor((ct*60+cm+cookTime)/60);
-const em=(ct*60+cm+cookTime)%60;
-
-console.log(et >= 24 ? et % 24:et, em);
+console.log(end_h % 24 + " " + end_m);
