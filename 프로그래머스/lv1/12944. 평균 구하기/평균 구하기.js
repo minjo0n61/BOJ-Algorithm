22e5ef311437be4d1be04df988c27243b6eab850
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-02 10:21:58 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,9 +1,8 @@
 function solution(arr) {
     let answer = 0;
     let sum = 0;
-    
-    for (let i=0; i<arr.length; i++){
-        sum += arr[i];
+    for (let i=0;i<arr.length;i++){
+        sum +=arr[i];
     }
     return answer=sum/arr.length;
 }
