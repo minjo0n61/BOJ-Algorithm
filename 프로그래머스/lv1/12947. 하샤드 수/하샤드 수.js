329947015e2bbe72e4975d274943aaca22fa0fe9
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-02 10:26:26 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,10 +1,9 @@
 function solution(x) {
     let answer = true;
-    let arr = x.toString()
-    let sum =0;
-    
-    for (let i = 0; i<arr.length; i++){
-        sum += Number(arr[i])
+    let arr = x.toString();
+    let sum = 0;
+    for (let i =0;i<arr.length;i++){
+        sum += Number(arr[i]);
     }
-    return answer=(x%sum===0)?true:false;
+    return answer=(x%sum===0?true:false);
 }
