cd29bda2e3e48aa11cf69a8f0b22a754126972a5
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-02 09:55:26 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,8 +1,7 @@
 function solution(seoul) {
     let answer = '';
-    
-    for(let i = 0; i<seoul.length;i++){
-        if(seoul[i]==="Kim"){return answer =`김서방은 ${i}에 있다`}
+    for(let i = 0; i<seoul.length; i++){
+        if(seoul[i]==='Kim'){return `김서방은 ${i}에 있다`}
     }
     return answer;
 }
