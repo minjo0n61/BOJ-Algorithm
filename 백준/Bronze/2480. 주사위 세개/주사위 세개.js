08a58e9ac8dea06747ed4f1e40053347cd03bf70
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-04 16:26:38 +0900

    [Bronze IV] Title: 주사위 세개, Time: 116 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -1,17 +1,21 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ");
-const A = Number(input[0]);
-const B = Number(input[1]);
-const C = Number(input[2]);
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
 
-if (A === B && A === C && B === C) { console.log(10000 + 1000 * A) }
-
-if (A !== B || A !== C || B !== C) {
-    if (A == B || A == C) { console.log(1000 + 100 * A) }
-    if (B === C) { console.log(1000 + 100 * B) }
+function dicemoney(a, b, c) {
+    if (a === b && a === c && b === c) {
+        return console.log(10000 + 1000 * a);
+    } else if (a === b || a === c || b === c) {
+        if (a === b || a === c) {
+            return console.log(1000 + 100 * a);
+        } else if (b === c) {
+            return console.log(1000 + 100 * b);
+        }
+    } else {
+        let arr = [];
+        arr.push(a, b, c);
+        console.log(Number(arr.sort().pop() * 100));
+    }
 }
-
-let arr = [A, B, C];
-if (A !== B && A !== C && B !== C) {
-    const sort = arr.sort();
-    console.log(sort.pop() * 100);
-}
+dicemoney(a, b, c)
