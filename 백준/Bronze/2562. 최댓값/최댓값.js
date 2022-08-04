f78e7f1f8661676eb0da1889ebbae4ca4d492705
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-04 17:37:04 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,3 +1,3 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n").map(Number);
-console.log(Math.max(...input));
-console.log(input.indexOf(Math.max(...input)) + 1);
+console.log(Math.max(...input))
+console.log(input.indexOf(Math.max(...input)) + 1)
