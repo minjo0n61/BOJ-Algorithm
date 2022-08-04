e9a1e342b5a3e5b00823bd8c63efd4a06be5e8f5
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-04 18:26:19 +0900

    [Bronze II] Title: OX퀴즈, Time: 136 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
@@ -1,15 +1,13 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
-let testcase = Number(input[0]);
-let quiz_score = 0;
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const testcase = input[0];
 
 for (let i = 1; i <= testcase; i++) {
-    let quiz = input[i].split("X");
-    //console.log(quiz)
-    for (let j = 0; j < quiz.length; j++) {
-        for (let h = 1; h <= quiz[j].length; h++) {
-            quiz_score += h;
+    let score = input[i].split("X");
+    let sum = 0;
+    for (let j = 0; j < score.length; j++) {
+        for (let h = 1; h <= score[j].length; h++) {
+            sum += h;
         }
     }
-    console.log(quiz_score)
-    quiz_score = 0;
+    console.log(sum);
 }
