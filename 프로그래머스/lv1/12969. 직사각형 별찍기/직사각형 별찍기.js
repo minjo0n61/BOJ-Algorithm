44907e7b391f6152cfc87d7b51362957d8195150
MinJoon_Choi <76935187+minjo0n61@users.noreply.github.com>
2022-08-20 02:34:51 +0900

    [level 1] Title: 직사각형 별찍기, Time: 51.43 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,6 +2,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-   
-    console.log(("*".repeat(a)+"\n").repeat(b).trim());
+    for(let i =0; i<b; i++){
+        console.log("*".repeat(a));    
+    }
 });
