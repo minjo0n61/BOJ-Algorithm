541374d89cd2ffa9fc6c218c6fbeecaf746a5493
MinJoon_Choi <minjoon1998@gmail.com>
2022-08-21 02:24:57 +0900

    [level 2] Title: 숫자의 표현, Time: 2.25 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    let answer = 0;
+    let sum = 0;
+    for(let i=1; i<=n;i++){
+        for(let j=i;j<=n;j++){
+            sum+=j;
+            if(sum===n){
+                answer+=1;
+                sum=0;
+                break;
+            }
+            if(sum>n){
+                sum=0;
+                break;
+            }
+        }
+    }
+    return answer;
+}
