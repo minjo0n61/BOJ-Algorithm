b96d3eb427a5b53e1316c409e6de1e6deefb0bbe
MinJoon_Choi <minjoon1998@gmail.com>
2022-09-17 00:33:08 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     var answer = [];
-    let strNum = n.toString();
-    for(let i=strNum.length-1;i>=0;i--){
+    let strNum = n.toString().split("");
+    for(let i =strNum.length-1; i>=0;i--){
         answer.push(Number(strNum[i]));
     }
     return answer;
