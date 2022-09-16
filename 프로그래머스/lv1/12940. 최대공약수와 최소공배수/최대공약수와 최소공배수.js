037047581ecfc3e3f6b750bf0967fbd69948cbea
MinJoon_Choi <minjoon1998@gmail.com>
2022-09-17 03:39:02 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,8 +1,9 @@
 function solution(n, m) {
     var answer = [];
-    function gcd(a,b){
-        if(b===0){return a;}
-        return gcd(b,a%b);
+    function getGcd(a,b){
+        if(b===0){return a}
+        return getGcd(b,a%b);
     }
-    return answer=[gcd(n,m),n*m/gcd(n,m)];
+    answer.push(getGcd(n,m),n*m/getGcd(n,m));
+    return answer;
 }
