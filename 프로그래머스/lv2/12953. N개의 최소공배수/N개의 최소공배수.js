25c396473bf895e37ef852e76bef16012d461634
MinJoon_Choi <minjoon1998@gmail.com>
2022-09-17 13:57:40 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,8 +1,8 @@
-function gcd(a,b){
-    if(b===0){return a;}
-    return gcd(b,a%b);
+function getGcd(a,b){
+    if(b===0){return a}
+    return getGcd(b,a%b)
 }
 
-function solution(arr){
-    return arr.reduce((a,b)=>a*b/gcd(a,b));
+function solution(arr) {
+    return arr.reduce((a,b)=>a*b/getGcd(a,b));
 }
