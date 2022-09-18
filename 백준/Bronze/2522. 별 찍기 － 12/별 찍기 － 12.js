65360263cf241620d6470fcfd17417666fcbaaad
MinJoon_Choi <minjoon1998@gmail.com>
2022-09-18 21:11:48 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 136 ms, Memory: 9692 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin');
+
+for (let i = 1; i <= +input; i++) {
+    console.log(" ".repeat(input - i) + "*".repeat(i))
+}
+for (let i = input - 1; i > 0; i--) {
+    console.log(" ".repeat(input - i) + "*".repeat(i))
+}
