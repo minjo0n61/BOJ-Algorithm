e8eb9122d7c2fbd6f3f7f2041c38588642eba690
MinJoon_Choi <minjoon1998@gmail.com>
2022-10-28 18:21:16 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 148 ms, Memory: 11180 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
@@ -1,20 +1,18 @@
-const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map(e => e.split(" ").map(Number));
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map(e => e.split(" ").map(x => +x));
 
 const [n, m] = input.shift();
 let arr = new Array(n).fill(0).map(() => new Array(m).fill(0));
 
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-        arr[i][j] = input[i][j] + input[i + n][j];
+        arr[i][j] = input[i][j] + input[i + n][j]
     }
 }
 let answer = '';
-
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
         answer += arr[i][j].toString() + " ";
     }
     answer += "\n";
 }
-
-console.log(answer);
+console.log(answer.trim());
