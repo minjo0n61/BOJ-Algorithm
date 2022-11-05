19b0df48771cacef9da1735618b191ef8ada4d58
MinJoon_Choi <minjoon1998@gmail.com>
2022-11-05 15:52:53 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 10076 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.js
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -1,22 +1,24 @@
-const input = require('fs').readFileSync(process.platform === "linux"?"/dev/stdin":"./input.txt").toString().trim().split("\n").map((el)=>el.split(" ").map((el)=>+el));
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map((el) => el.split(" ").map((el) => +el));
 const testCase = input.shift();
-let arr = new Array(100).fill().map(()=>new Array(100).fill(false));
-for(let i = 0; i<testCase; i++){
+let arr = new Array(100).fill().map(() => new Array(100).fill(false));
+
+for (let i = 0; i < testCase; i++) {
     let x = input[i][0];
     let y = input[i][1];
-    
-    for(let j = 0; j<10; j++){
-        for(let k = 0; k<10; k++){
-            arr[x+j][y+k]=true;
+
+    for (let j = 0; j < 10; j++) {
+        for (let k = 0; k < 10; k++) {
+            arr[x + j][y + k] = true;
         }
     }
 }
-let answer = arr.reduce((past, current)=>{
-    for(let el of current){
-        if(el === true){
+let answer = arr.reduce((past, current) => {
+    for (let el of current) {
+        if (el) {
             past++;
         }
     }
     return past;
-},0)
+}, 0);
+
 console.log(answer);
