bafc33d0f2f1b91a8df7f4e7ae73a5f3e43c03f0
MinJoon_Choi <minjoon1998@gmail.com>
2022-11-08 19:51:19 +0900

    [Silver V] Title: 나이순 정렬, Time: 432 ms, Memory: 48124 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,5 +1,3 @@
-// 나이 먼저 비교 후 이름 순 비교
-
 const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map((el) => el.split(" "));
 const testCase = +input.shift();
 let answer = '';
@@ -7,12 +5,7 @@ input.sort((a, b) => {
     if (a[0] !== b[0]) {
         return a[0] - b[0];
     }
-    // else{
-    //     return a[1]
-    // }
 })
-// input.sort()
-// console.log(input)
 for (let i = 0; i < testCase; i++) {
     let age = +input[i][0];
     let name = input[i][1];
