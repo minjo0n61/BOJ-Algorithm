e1546c656c9fbe463b851109d34e44429911621c
MinJoon_Choi <minjoon1998@gmail.com>
2022-11-15 17:56:29 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 216 ms, Memory: 10420 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
@@ -1,16 +1,17 @@
 const input = require('fs').readFileSync(process.platform === "linux"?"/dev/stdin":"./input.txt").toString().trim().split("\n");
-[size,...arr]=input;
-[row,col]=size.split(" ");
-arr = arr.map(el=>el.trim().split(""));
+[size,...arr]= input;
+[row, col]= size.split(" ").map(Number);
+arr = arr.map(el => el.trim().split(""));
 
 const answer =[];
-const line = ["WBWBWBWB","BWBWBWBW"];
+const line =["WBWBWBWB","BWBWBWBW"]
 
-for(let i=0; i<=row-8; i++){
+for(let i=0 ; i<=row-8; i++){
     for(let j=0; j<=col-8; j++){
         
         for(let k=0; k<line.length; k++){
             let count =0;
+            
             for(let x=0; x<8; x++){
                 for(let y=0; y<8; y++){
                     const current = arr[i+x][j+y];
