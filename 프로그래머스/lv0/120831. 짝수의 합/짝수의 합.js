7400a2e5ad6442295211353772f50cd125f8d590
MinJoon_Choi <minjoon1998@gmail.com>
2022-11-15 20:03:32 +0900

    [level 0] Title: 짝수의 합, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let arr=[];
+    for(let i =1; i<=n; i++){
+        if(i%2===0){
+            arr.push(i);
+        }
+    }
+    return arr.reduce((pv, cv)=>pv+cv,0);
+}
