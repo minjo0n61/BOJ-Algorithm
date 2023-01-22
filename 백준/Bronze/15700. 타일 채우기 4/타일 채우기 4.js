ce1adc60960257e234d76398793392cd104d0390
MinJoon_Choi <minjoon1998@gmail.com>
2023-01-22 09:06:03 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 168 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.js
@@ -0,0 +1,2 @@
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(BigInt);
+console.log((BigInt(input[0] * input[1] / 2n)).toString());
