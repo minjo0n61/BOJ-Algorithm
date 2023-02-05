3c6cb85c5c8b74b542b786d9099c29aa5e4a715f
MinJoon_Choi <minjoon1998@gmail.com>
2023-02-06 03:45:43 +0900

    [Silver III] Title: N과 M (2), Time: 140 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,23 @@
+const { resourceUsage } = require('process');
+
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(Number);
+let [N, M] = input;
+let result = [];
+let answer = [];
+let cheak = new Array(N + 1).fill(0);
+
+function recur(num, start) {
+    if (num === M) {
+        return console.log(result.join(" "));
+    }
+    for (let i = start; i <= N; i++) {
+        if (cheak[i] === 0) {
+            cheak[i] = 1;
+            result.push(i);
+            recur(num + 1, i);
+            cheak[i] = 0;
+            result.pop();
+        }
+    }
+}
+recur(0, 1);
