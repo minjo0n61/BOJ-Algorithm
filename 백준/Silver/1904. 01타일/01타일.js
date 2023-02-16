9b6eb4732514e73476b579529d3805836fc5e628
MinJoon_Choi <minjoon1998@gmail.com>
2023-02-16 19:45:25 +0900

    [Silver III] Title: 01타일, Time: 196 ms, Memory: 29360 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.js
@@ -0,0 +1,11 @@
+const input = +require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+
+let memo = new Array(input).fill(0);
+memo[1] = 1;
+memo[2] = 2;
+memo[3] = 3;
+
+for (let i = 4; i <= input; i++) {
+    memo[i] = (memo[i - 1] + memo[i - 2]) % 15746;
+}
+console.log(memo[input]);
