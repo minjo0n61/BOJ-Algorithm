af9d63f959b27fea4ea44c170577b40a3645da28
MinJoon_Choi <minjoon1998@gmail.com>
2023-07-13 19:18:17 +0900

    [Bronze IV] Title: 모비스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28074. 모비스/모비스.py
@@ -0,0 +1,13 @@
+MOBIS = ['M', 'O', 'B', 'I', 'S']
+word = input()
+result = True
+
+for i in MOBIS :
+    if i not in word :	
+        result = False
+        break
+
+if result :
+    print('YES')
+else :
+    print('NO')
