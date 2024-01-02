d918dca3b1f51946c17a2ce8d9be07083b96d442
MinJoon_Choi <minjoon1998@gmail.com>
2024-01-02 18:47:24 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -1,8 +1,17 @@
-const input = require('fs').readFileSync('/dev/stdin');
+const input = +require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
 
-function fibonacci(n) {
-    if (n <= 1) { return n; }
-    return fibonacci(n - 2) + fibonacci(n - 1);
+let arr = [];
+Fibonacci(input);
+console.log(arr[arr.length - 1]);
+
+function Fibonacci(num) {
+    for (let i = 0; i <= num; i++) {
+        if (i === 0) {
+            arr.push(0);
+        } else if (i === 1 || i === 2) {
+            arr.push(1);
+        } else {
+            arr.push(arr[i - 2] + arr[i - 1]);
+        }
+    }
 }
-
-console.log(fibonacci(+input));
