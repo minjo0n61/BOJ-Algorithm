331f6a93b15c3002120841568971c90f9a3b891a
MinJoon_Choi <minjoon1998@gmail.com>
2024-01-08 04:27:52 +0900

    [Silver III] Title: queuestack, Time: 304 ms, Memory: 44632 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.js
+++ 백준/Silver/24511. queuestack/queuestack.js
@@ -1,18 +1,21 @@
-let [N, s, arr, M, q] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
-s = s.split(' ').map(Number);
-arr = arr.split(' ').map(Number);
-q = q.split(' ').map(Number);
-res = [];
-let index = N-1;
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map(el => el.split(" ").map(Number));
+let N = input.shift()[0];
+let A = input.shift();
+let B = input.shift();
+let M = input.shift()[0];
+let C = input.shift();
+let result = [];
+
 let i = 0;
-while(res.length < M) {
-    if(index >= 0) {
-        if(s[index] == 0) res.push(arr[index]);
-        index -= 1;
-    }
-    else {
-        res.push(q[i])
+while (result.length < M) {
+    if (N - 1 >= 0) {
+        if (A[N - 1] === 0) {
+            result.push(B[N - 1]);
+        }
+        N -= 1;
+    } else {
+        result.push(C[i]);
         i += 1;
     }
 }
-console.log(res.join(' '))
+console.log(result.join(" "));
