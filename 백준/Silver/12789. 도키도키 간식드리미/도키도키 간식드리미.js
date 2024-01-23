50bc6e05eaa15932603e6a47bc7ab3f98cfbc2a5
MinJoon_Choi <minjoon1998@gmail.com>
2024-01-23 13:27:23 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 120 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.js
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.js
@@ -1,30 +1,30 @@
-const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map(el => el.split(" ").map(Number));
-let testNum = input.shift()[0];
-let testCase = input.shift();
-let result = [];
-let stack = [];
+let [N, arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+N = +N;
+arr = arr.split(" ").map(Number);
+let top = 0; // 스택의 맨 위를 가리키는 변수
+let cur = 1; // 현재 기대값
+const stack = []; // 순서가 아닌 사람들 임시 저장
+let output = '';
 
-stackLine(testCase);
+arr.forEach((e) => {
+  while (stack[top-1] === cur) {
+    stack.pop();
+    top--;
+    cur++;
+  }
 
-function stackLine(testCase) {
-    let i = 1;
-    while (result.length !== testNum) {
-        if (stack.length && stack[stack.length - 1] == i) {
-            result.push(stack.pop());
-            i++;
-        }
-        else {
-            let nextNum = testCase.shift();
-            if (nextNum == undefined) {
-                return console.log("Sad");
-            }
-            else if (nextNum == i) {
-                result.push(nextNum);
-                i++;
-            } else {
-                stack.push(nextNum);
-            }
-        }
-    }
-    return console.log("Nice")
+  if (e !== cur) {
+    stack.push(e);
+    top++;
+  } else {
+    cur++;
+  }
+});
+
+while (stack.pop() === cur) {
+  top--;
+  cur++;
 }
+
+top === 0 ? (output = 'Nice') : (output = 'Sad');
+console.log(output);
