2b0c6769ed9550b856419dc012d2308d76dd3342
MinJoon_Choi <minjoon1998@gmail.com>
2024-01-24 12:49:03 +0900

    [Silver I] Title: 정수 삼각형, Time: 204 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -1,15 +1,14 @@
-const input = require('fs').readFileSync(process.platform === "linux"?"/dev/stdin":"./input.txt").toString().trim().split("\n").map(el=>el.split(" ").map(Number));
-let floor = +input.shift();
+const [floor, ...input] = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map(el => el.split(" ").map(Number));
 
-for(let i=1; i<floor; i++){
-    for(let j=0; j<input[i].length; j++){
-        if(j===0){
-            input[i][j] += input[i-1][j];
-        }else if(j===input[i].length-1){
-            input[i][j] += input[i-1][j-1];
-        }else{
-            input[i][j] += Math.max(input[i-1][j-1], input[i-1][j]);
+for (let i = 1; i < floor; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+        if (j === 0) {
+            input[i][j] += input[i - 1][j];
+        } else if (j === input[i].length - 1) {
+            input[i][j] += input[i - 1][j - 1];
+        } else {
+            input[i][j] += Math.max(input[i - 1][j - 1], input[i - 1][j]);
         }
     }
 }
-console.log(Math.max(...input[floor-1]));
+console.log(Math.max(...input[floor - 1]));
