08c5cfe3c45e1c9add11cd07c378e64ba75f4a73
MinJoon_Choi <minjoon1998@gmail.com>
2024-02-09 12:53:00 +0900

    [Bronze IV] Title: DKSH 찾기, Time: 128 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.js
@@ -0,0 +1,12 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString();
+
+let result = 0;
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "D" && input[i + 1] === "K" && input[i + 2] === "S" && input[i + 3] === "H") {
+    result++;
+  }
+}
+
+console.log(result);
