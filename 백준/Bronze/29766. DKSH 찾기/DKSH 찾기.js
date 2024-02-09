a6e45affe2746edd08f825884cb552559ab187be
MinJoon_Choi <minjoon1998@gmail.com>
2024-02-09 12:53:43 +0900

    [Bronze IV] Title: DKSH 찾기, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.js
+++ 백준/Bronze/29766. DKSH 찾기/DKSH 찾기.js
@@ -6,6 +6,7 @@ let result = 0;
 for (let i = 0; i < input.length; i++) {
   if (input[i] === "D" && input[i + 1] === "K" && input[i + 2] === "S" && input[i + 3] === "H") {
     result++;
+    i += 3;
   }
 }
 
