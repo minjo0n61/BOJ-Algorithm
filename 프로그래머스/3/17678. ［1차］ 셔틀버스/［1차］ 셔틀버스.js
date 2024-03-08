016a021d346369933b10021f59b4335be6caa638
최민준 <minjoon1998@gmail.com>
2024-03-08 10:56:21 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.67 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
@@ -1,43 +1,86 @@
 function solution(n, t, m, timetable) {
-  let ans;
-  time = [];
-  const lastTimeMinute = Number(9 * 60 + (n - 1) * t);
-  for (let i of timetable) {
-    const x = Number(i.slice(0, 2)) * 60 + Number(i.slice(3, 5));
-    if (lastTimeMinute >= x) {
-      time.push(x);
-    }
-  }
-  time.sort((a, b) => a - b);
+  let answer = 0;
+  let firstBusStartTime = 540;
+  let lastfirstBusStartTime = firstBusStartTime + (n - 1) * t;
+  let times = timetable.map((el) => el.split(":").map(Number)).map((el) => el[0] * 60 + el[1]);
+  times.sort((a, b) => a - b);
+  // console.log(times);
 
-  let p = 0;
+  let busSchedule = [{ time: 540, man: [] }];
+  for (let i = 0; i < n - 1; i++) {
+    busSchedule.push({ time: firstBusStartTime + t * (i + 1), man: [] });
+  }
+  // console.log(busSchedule);
+
   let count = 0;
-  let busTime = 9 * 60;
-  let timeStamp;
+  for (let i = 0; i < n; i++) {
+    let presentBus = busSchedule[i];
+    let presentBusTime = presentBus.time;
+    let presentBusMan = presentBus.man;
 
-  while (busTime <= lastTimeMinute && p < timetable.length) {
-    if (time[p] <= busTime && count < m) {
-      if (busTime === lastTimeMinute && count === m - 1) {
-        timeStamp = time[p];
-        break;
+    for (let j = 0; j < m; j++) {
+      if (presentBusTime >= times[count]) {
+        presentBusMan.push(times[count]);
+        count++;
+        if (!times[count] && count < m * n) {
+          break;
+        }
       }
-      count += 1;
-      p += 1;
-    } else {
-      busTime += t;
-      count = 0;
     }
   }
 
-  if (timeStamp) {
-    ans = timeStamp - 1;
-  } else {
-    ans = lastTimeMinute;
+  let lastBus = busSchedule[busSchedule.length - 1];
+  if (lastBus.man.length === m) {
+    answer = lastBus.man[lastBus.man.length - 1] - 1;
   }
+  if (lastBus.man.length < m) {
+    answer = lastBus.time;
+  }
+  // console.log(lastBus.man[lastBus.man.length - 1] - 1);
+  // console.log(lastBus);
 
-  let hour = parseInt(ans / 60);
-  let minute = ans % 60;
-  if (hour < 10) hour = "0" + String(hour);
-  if (minute < 10) minute = "0" + String(minute);
+  let hour = Math.floor(answer / 60)
+    .toString()
+    .padStart(2, "0");
+  let minute = Math.floor(answer % 60)
+    .toString()
+    .padStart(2, "0");
   return `${hour}:${minute}`;
 }
+
+// let [n, t, m] = [1, 1, 5];
+// let timetable = ["08:00", "08:01", "08:02", "08:03"];
+
+// let [n, t, m] = [2, 10, 2];
+// let timetable = ["09:10", "09:09", "08:00"];
+
+// let [n, t, m] = [2, 1, 2];
+// let timetable = ["09:00", "09:00", "09:00", "09:00"];
+
+// let [n, t, m] = [1, 1, 5];
+// let timetable = ["00:01", "00:01", "00:01", "00:01", "00:01"];
+
+// let [n, t, m] = [1, 1, 1];
+// let timetable = ["23:59"];
+
+// let [n, t, m] = [10, 60, 45];
+// let timetable = [
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+//   "23:59",
+// ];
+
+// console.log(solution(n, t, m, timetable));
