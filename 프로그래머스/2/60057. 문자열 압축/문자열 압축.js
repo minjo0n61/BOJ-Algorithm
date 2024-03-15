ae20d3432912d74b57506ca07fd82a64b9099fca
최민준 <minjoon1998@gmail.com>
2024-03-15 16:59:35 +0900

    [level 2] Title: 문자열 압축, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,4 +1,35 @@
 function solution(s) {
-    var answer = 0;
-    return answer;
+  let answer = s.length;
+
+  for (let i = 1; i <= parseInt(s.length / 2); i++) {
+    let string = "";
+    let count = 1;
+    let cutString = s.substring(0, i);
+    let idx = 0;
+
+    for (idx = i; idx <= s.length; idx += i) {
+      let nextString = s.substring(idx, i + idx);
+
+      if (cutString === nextString) {
+        count += 1;
+      } else {
+        if (count === 1) {
+          string = string + cutString;
+        } else {
+          string = string + count + cutString;
+        }
+
+        count = 1;
+        cutString = nextString;
+      }
+    }
+    if (count === 1) {
+      string = string + cutString;
+    } else {
+      string = string + count + cutString;
+    }
+    answer = Math.min(answer, string.length);
+  }
+
+  return answer;
 }
