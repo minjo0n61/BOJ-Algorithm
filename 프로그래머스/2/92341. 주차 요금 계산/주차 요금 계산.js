1776dcf955d511a62d766fd53b8b1d21586ad7c8
최민준 <minjoon1998@gmail.com>
2024-03-20 18:58:43 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,4 +1,46 @@
 function solution(fees, records) {
-    var answer = [];
-    return answer;
+  let totalTimes = calculateTotalTimes(records);
+  let answer = Object.keys(totalTimes)
+    .sort((a, b) => a - b)
+    .map((carNumber) => calculateFee(fees, totalTimes[carNumber]));
+  return answer;
+}
+
+function makeMinute(time) {
+  const [hour, minute] = time.split(":").map(Number);
+  return hour * 60 + minute;
+}
+
+function calculateFee(fees, totalMinutes) {
+  const [basicTime, basicFee, unitTime, unitFee] = fees;
+  if (totalMinutes <= basicTime) return basicFee;
+  return basicFee + Math.ceil((totalMinutes - basicTime) / unitTime) * unitFee;
+}
+
+function calculateTotalTimes(records) {
+  let recordsObj = {};
+
+  records.forEach((record) => {
+    const [time, carNumber, status] = record.split(" ");
+    const minutes = makeMinute(time);
+    if (!recordsObj[carNumber]) recordsObj[carNumber] = [];
+    recordsObj[carNumber].push({ minutes, status });
+  });
+
+  let totalTimes = {};
+
+  for (const carNumber in recordsObj) {
+    const lastRecord = recordsObj[carNumber][recordsObj[carNumber].length - 1];
+    if (lastRecord.status === "IN") {
+      recordsObj[carNumber].push({ minutes: makeMinute("23:59"), status: "OUT" });
+    }
+
+    let totalTime = 0;
+    for (let i = 0; i < recordsObj[carNumber].length; i += 2) {
+      totalTime += recordsObj[carNumber][i + 1].minutes - recordsObj[carNumber][i].minutes;
+    }
+    totalTimes[carNumber] = totalTime;
+  }
+
+  return totalTimes;
 }
