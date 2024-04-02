0b1d0aaa3f2894a3a5c0f53fee899b60c1994f9e
최민준 <minjoon1998@gmail.com>
2024-04-03 01:34:45 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,10 +1,22 @@
 function solution(my_string) {
-    var answer = 0;
-    
-    for(let i=0; i<my_string.length; i++){
-        if(isNaN(my_string[i])){}
-        console.log(my_string[i])
+  var answer = 0;
+  let arr = [];
+
+  for (let i = 0; i < my_string.length; i++) {
+    let num = [];
+    if (!isNaN(my_string[i])) {
+      num.push(my_string[i]);
+      let idx = 1;
+      while (true) {
+        if (!isNaN(my_string[i + idx])) {
+          num.push(my_string[i + idx]);
+          i++;
+        } else {
+          break;
+        }
+      }
+      arr.push(+num.join(""));
     }
-    
-    return answer;
+  }
+  return (answer = arr.reduce((a, b) => a + b, 0));
 }
