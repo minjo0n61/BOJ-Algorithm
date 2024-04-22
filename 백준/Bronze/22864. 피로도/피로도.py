6927e0071e372f249d63444b23c608fd88baf296
최민준 <minjoon1998@gmail.com>
2024-04-22 11:47:01 +0900

    [Bronze II] Title: 피로도, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/22864. 피로도/피로도.py
@@ -0,0 +1,20 @@
+import sys
+
+A,B,C,M = map(int, sys.stdin.readline().split())
+
+T = 0
+DW = 0
+
+for i in range(24):
+  if T > M:
+    print(0)
+  else:
+    if T + A <= M:
+      T += A
+      DW += B
+    else:
+      if T - C >=0:
+        T -= C
+      else:
+        T = 0
+print(DW)
